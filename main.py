4b99568dbe1f1c73ccaee28ba58411f34dbfd20d
BungaEdu <eduardoflores.9595@gmail.com>
2023-01-10 02:10:33 +0100

    abrir excel y abrir navegador, falta hacerlos méto

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-def prueba(num):
-    print(num)
-
+import pandas as pd
+import webbrowser
+from urllib.request import urlopen
 
 def main():
-    num=7
-    prueba(num)
+    doc = pd.read_excel("C:/Users/Edu guapo/Desktop/afueraEstudios/TFG_enlaces.xls", header =None)
+
+    dimension = doc.shape
+    tamFilas = dimension[0]
+    print(f"tamFilas: ", tamFilas)
+
+    arrayEnlaces = []
+    
+    for x in range (0,tamFilas):
+        arrayEnlaces.append(doc.iloc[x].to_string(index=False))
+        print('"'+arrayEnlaces[x]+'"')
+        webbrowser.open(arrayEnlaces[x], new=0, autoraise=True)
+
+    # url = "https://twitter.com/SoyHijoDelPerro"
+    # webbrowser.open(url, new=0, autoraise=True) 
 
 if __name__=='__main__':
     main()
