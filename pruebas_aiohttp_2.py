5c380e88abe4b2c87698e3d9b60101713b7565c6
BungaEdu <eduardoflores.9595@gmail.com>
2023-04-07 02:24:52 +0200

    .

--- pruebas_aiohttp_2.py
+++ pruebas_aiohttp_2.py
@@ -6,11 +6,11 @@ async def main():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://es.lyricstraining.com/') as response:
 
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
+            print("XXXX-Status:", response.status)
+            print("XXXX-Content-type:", response.headers['content-type'])
 
             html = await response.text()
-            print("Body:", html[:15], "...")
+            print("Body:", html[:20], "...")
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
